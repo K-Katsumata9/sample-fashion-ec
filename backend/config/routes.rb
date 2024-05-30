cd9002f1ec145d1ff02139a42823553ecb78f2aa
K-Katsumata9 <dowbler@icloud.com>
2024-05-30 21:38:57 +0900

    add Stripe function

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+	
   root "products#index"
 
 	resources :products, only: [:show]
 	resources :line_items, only: [:create, :destroy]
 	resources :carts, only: [:index]
+
+	namespace :payments do
+    get 'checkout'
+    get 'success'
+  end
+
 end
